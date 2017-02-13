60c39a7ec05b0d05856ed1b248431126ce4c21dd
110Percent <its.a.me.110percent@gmail.com>
2017-02-12 22:25:07 -0500

    Pi Commit

--- beheeyem.js
+++ beheeyem.js
@@ -829,6 +829,32 @@ beheeyem.on("message", msg => {
                     msg.channel.sendMessage("Sorry! No logged edits were found.");
                 }
             }
+            else if (cmd == "roll"){
+                var rnd = null;
+                var dice = [];
+                if (args){
+                    if (args.indexOf("d") == -1){
+                        if (args == Number(args)){
+                            rnd = Math.ceil(Math.random() * Number(args));
+                        }
+                    }
+                    else {
+                        var numDice = (args.split("d")[0]);
+                        for (var i = 0; i < numDice; i++){
+                            dice.push(Number(Math.ceil(Math.random() * Number(args.split("d")[1]))));
+                        }
+                    }
+                }
+                else {
+                    rnd = Math.ceil(Math.random() * 6);
+                }
+                if (dice.length > 0) {
+                    msg.channel.sendMessage("Rolled `" + dice.join("` `") + "`.");
+                }
+                else if (rnd != null){
+                    msg.channel.sendMessage("Rolled `" + rnd + "`");
+                }
+            }
             else if (cmd == "nonya"){
                 var nonyas = require("./data/nonyas.json").nonyas;
                 var selectedNonya = nonyas[Math.floor(Math.random() * nonyas.length)];
