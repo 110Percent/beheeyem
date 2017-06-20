f3f82feaed9df4c1508f8f105896f5ea92c6b133
Curtis <its.a.me.110percent@gmail.com>
2017-06-20 10:48:56 -0400

    Added additional nonyas

--- commands/nonya.js
+++ commands/nonya.js
@@ -1,5 +1,5 @@
 exports.action = (msg, args) => {
-    var nonyas = require("./data/nonyas.json").nonyas;
+    var nonyas = require("../data/nonyas.json").nonyas;
     var selectedNonya = nonyas[Math.floor(Math.random() * nonyas.length)];
     msg.channel.sendFile(selectedNonya, "nonya." + selectedNonya.split(".")[selectedNonya.split(".").length - 1]);
 }
