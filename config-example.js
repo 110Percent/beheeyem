10a6ec4f40c22c8a356a3a87c12ec007aff685f9
Curtis <its.a.me.110percent@gmail.com>
2017-09-02 06:52:30 -0400

    Reset gh-pages branch

--- config-example.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    token: 'p.LEasee-nt-eRYouRt_o.KENhe-Re',
-    prefix: 'b.',
-    dbotsToken: "If you're in the Discord Bots server and want to add your bot's server count, put your authorization key in here."
-}
