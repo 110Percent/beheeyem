2606ed9ce0d2a6a80e7f5d513bb1c6b2f84d260e
Curtis <its.a.me.110percent@gmail.com>
2017-09-05 18:01:21 -0400

    Create info command

---
+++ commands/info.js
@@ -0,0 +1,3 @@
+exports.action = (msg) => {
+    msg.channel.send('Hello, and thank you for choosing Beheeyem! This guy is your go-to resource for all things Pokémon-related.\nFor a list of useful commands, type `b.help`. For more information, visit http://beheeyem.110percent.xyz .\n<:beeFace:328348788202799109>❤');
+}
