a298f36bd05f0aa8abf82a2e7b88c36fb28fd6c9
Curtis <its.a.me.110percent@gmail.com>
2017-09-13 22:14:49 -0400

    Added spellcheck and online database fetching. Also added aliases.

--- commands/ability.js
+++ commands/ability.js
@@ -1,17 +1,43 @@
-const abilities = require("../data/abilities.js").BattleAbilities;
+const request = require('request');
+const requireFromUrl = require('require-from-url/sync');
+let abilities;
+const Matcher = require('did-you-mean');
+
+request('https://cdn.rawgit.com/Zarel/Pokemon-Showdown/2d605975/data/abilities.js', (err, res, body) => {
+    if (!err && res.statusCode == 200) {
+        abilities = requireFromUrl('https://cdn.rawgit.com/Zarel/Pokemon-Showdown/2d605975/data/abilities.js').BattleAbilities;
+    } else {
+        console.log('Error fetching Showdown abilities; Switching to local abilities...');
+        abilities = require('../data/abilities.js').BattleAbilities;
+    }
+    match = new Matcher(Object.keys(abilities).join(' '));
+});
+request('https://cdn.rawgit.com/Zarel/Pokemon-Showdown/2d605975/data/aliases.js', (err, res, body) => {
+    if (!err && res.statusCode == 200) {
+        aliases = requireFromUrl('https://cdn.rawgit.com/Zarel/Pokemon-Showdown/2d605975/data/aliases.js').BattleAliases;
+    } else {
+        console.log('Error fetching Showdown aliases; Switching to local aliases...');
+        aliases = require('../data/aliases.js').BattleAliases;
+    }
+});
 
 exports.action = (msg, args) => {
+    let abilityName = args.toLowerCase();
+    if (aliases[abilityName]) {
+        abilityName = aliases[abilityName];
+    }
+    abilityName = abilityName.toLowerCase();
     for (var i = 0; i < Object.keys(abilities).length; i++) {
-        if (abilities[Object.keys(abilities)[i]].name.toLowerCase() == args.toLowerCase()) {
+        if (abilities[Object.keys(abilities)[i]].name.toLowerCase() == abilityName) {
             var ability = abilities[Object.keys(abilities)[i]];
             break;
         }
     }
-    var abilityDesc = ability.desc;
-    if (!abilityDesc) {
-        abilityDesc = ability.shortDesc;
-    }
     if (ability) {
+        var abilityDesc = ability.desc;
+        if (!abilityDesc) {
+            abilityDesc = ability.shortDesc;
+        }
         msg.channel.send("\\_\\_\\_\\_\\_\\_\\_\\_\\_\\_\\_\\_\\_\\_\\_\\_\\_\\_\\_\\_\\_\\_\\_\\_\\_\n\n**" + capitalizeFirstLetter(ability.name) + "**", {
             embed: {
                 color: 35071,
@@ -30,6 +56,15 @@ exports.action = (msg, args) => {
                 ]
             }
         });
+    } else {
+        let dym = match.get(args);
+        let dymString;
+        if (dym == null) {
+            dymString = 'Check your spelling and try again.';
+        } else {
+            dymString = `Did you mean \`${dym}\`?`;
+        }
+        msg.channel.send("⚠ Ability not found! " + dymString);
     }
 }
 
