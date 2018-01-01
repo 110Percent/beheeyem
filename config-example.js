63919d74551e63d6ce0162ac778ffa931a2d9bb5
Curtis <its.a.me.110percent@gmail.com>
2018-01-01 11:15:02 -0500

    Some commenting stuff and cleanup

--- config-example.js
+++ config-example.js
@@ -1,5 +1,5 @@
 module.exports = {
     token: 'p.LEasee-nt-eRYouRt_o.KENhe-Re',
     prefix: 'b.',
-    dbotsToken: "If you're in the Discord Bots server and want to add your bot's server count, put your authorization key in here."
+    ownerID: '1234567890'
 }
