ac7bf130578f2bc9e2a5ec7ee601e52a85973dea
Curtis <its.a.me.110percent@gmail.com>
2018-01-02 15:34:14 -0500

    Updated info command with patreon line

--- commands/info.js
+++ commands/info.js
@@ -1,3 +1,3 @@
 exports.action = (msg) => {
-    msg.channel.send('Hello, and thank you for choosing Beheeyem! This guy is your go-to resource for all things Pokémon-related.\nFor a list of useful commands, type `b.help`. For more information, visit http://beheeyem.110percent.xyz .\n<:beeFace:328348788202799109>❤');
+    msg.channel.send('Hello, and thank you for choosing Beheeyem! This guy is your go-to resource for all things Pokémon-related.\nFor a list of useful commands, type `b.help`. For more information, visit http://beheeyem.110percent.xyz .\nIf you like Beheeyem, please consider becoming a patron! You\'d be a big help to its development and keeping it working. <https://patreon.com/beheeyem>\n<:beeFace:328348788202799109>❤');
 }
