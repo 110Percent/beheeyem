71fb724ca4251d145759648b78827f288feda36c
Curtis <its.a.me.110percent@gmail.com>
2018-01-03 16:28:04 -0500

    Update diceroll command

--- commands/roll.js
+++ commands/roll.js
@@ -4,15 +4,25 @@ exports.action = (msg, args) => {
     if (args) {
         if (args.indexOf("d") == -1) {
             if (args == Number(args)) {
-                if (Number(args) > 0 && Number(args) < 100) {
+                if (Number(args) > 0 && Number(args) <= 100) {
                     rnd = Math.ceil(Math.random() * Number(args));
                 } else {
                     msg.channel.send('How are you even reading this die?');
                 }
+            } else {
+                msg.channel.send('That doesn\'t look like a positive integer to me. (I\'m not good at parsing words as numbers, sorry...)')
             }
         } else {
             var numDice = (args.split("d")[0]);
+            if (numDice != Number(numDice)) {
+                msg.channel.send('Sorry, what...?');
+                return;
+            }
             if (numDice < 16) {
+                if (args.split("d")[1] != Number(args.split("d")[1])) {
+                    msg.channel.send('What are these dice...?');
+                    return;
+                }
                 if (Number(args.split("d")[1]) > 0 && Number(args.split("d")[1]) < 100) {
                     for (var i = 0; i < numDice; i++) {
                         dice.push(Number(Math.ceil(Math.random() * Number(args.split("d")[1]))));
